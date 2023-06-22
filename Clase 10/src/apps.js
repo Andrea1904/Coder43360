1b45e6524eddb597d6359dbd25a30672e9e99464
Andrea1904 <alopezm@bancodeoccidente.com.co>
2023-06-22 17:02:19 -0500

    Add files via upload

--- Clase 10/src/apps.js
+++ Clase 10/src/apps.js
@@ -1,43 +1,28 @@
-import express from "express";
-import handlebars from "express-handlebars";
-import __dirname from "./utils.js";
-import viewRouter from "./routes/views.router.js"
-import { Server } from 'socket.io'
+import express from 'express';
+import __dirname from '../src/utils.js';
+import handlebars from 'express-handlebars';
+import viewRouter from './routes/view.router.js';
+import {Server} from 'socket.io';
 
-const app =express();
-const httpserver =app.listen(8080,()=>console.log("Server arriba"))
-
-const socketServer =new Server(httpserver)
+const app= express();
+const httpServer=app.listen(8080,()=>console.log("Server arriba"));
+const socketserver= new Server(httpServer);
 
 app.engine('handlebars',handlebars.engine());
-app.set('views',__dirname+'/views')
+app.set('views',__dirname+'/views');
 app.set('view engine','handlebars');
-app.use(express.static(__dirname+'/public'))
-app.use('/',viewRouter)
+app.use(express.static(__dirname + '/public'));
+app.use('/',viewRouter);
 
-const logs = []
-socketServer.on('connection',socket =>{
-    console.log("Inicio la comunicación")
+socketserver.on('connection',socket=>{
+    console.log("Tenemos un cliente conectado");
 
-        socket.on("message",data=>{
-        logs.push({socketid:socket.id,message:data})
-        socketServer.emit('log',{logs});
+    socket.on('message', data=>{
+        console.log(data)
     })
 })
 
-/*
-socketServer.on('connection',socket=>{
-    console.log("Nuevo cliente")
-
-    socket.on('message',data=>{
-        console.log(data)
-    })
-
-    socket.emit('evento_para_socket',"mensaje para que lo reciba el socket")
-})*/
+// on =escuchar /rcibir
+// emit=hablar /enviar
 
 
-/*
-socketSever.on //escuchar-recibir 
-socketSever.emit //hablar - enviar
-*/
